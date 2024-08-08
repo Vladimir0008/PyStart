4a54bb5c1f8c36f8087bec2d2099ab9db507a05a
Vladimir008 <vladimir.podkidyshev@gmail.com>
2024-08-08 21:43:44 +0300

    my third commit

--- hello.py
+++ hello.py
@@ -1,1 +1,49 @@
 print('Hello, World!')
+
+num = 10
+num += 1
+num -= 5
+print(num)
+res = 10 - 6 * 3 - 5
+print(res)
+
+tst = 1342453547656758678993553453 ** 5
+print(tst)
+
+aaa = 5 ** 3
+print(aaa)
+
+bbb = 7/2
+print(bbb)
+bbb = 7//2
+print(bbb)
+
+print(3 * 'abc') # выведет 'abcabcabc'
+
+str1 = 'ab'
+str2 = '12'
+print (2*str1 + 2* str2)
+
+txt = 'abcde'
+print(txt[-1]) # выведет 'e'
+print(txt[-4])
+
+tst = "abcd\"fr'23"
+print(tst)
+
+print("""
+1
+2
+
+3
+4""")
+
+isAdult = True # уже взрослый
+print(isAdult)
+
+test = None
+print(test) # выведет None
+
+tst1 = '123'
+tst2 = str(456)
+print(tst1 + tst2)
