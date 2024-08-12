429dd26bb65881d03506eece1df4c92679799905
Vladimir008 <vladimir.podkidyshev@gmail.com>
2024-08-12 21:31:05 +0300

    input and numbers and strings

---
+++ secondFile.py
@@ -0,0 +1,44 @@
+txt1 = '1'
+txt2 = '2'
+
+res = int(txt1) + int(txt2)
+
+print(res) # выведет число 3
+
+txt = '123'
+txt2 = '456'
+print(int(txt) + int(txt2))
+
+txt3 = '123456'
+result = (int(txt3[0]) + int(txt3[1]) + int(txt3[2]) + int(txt3[3]) + int(txt3[4]))
+print(result)
+
+txt4 = '2489'
+result = (int(txt4[2]) - int(txt4[1])) * int(txt4[0])
+print(result)
+
+num = 123
+txt1 = str(num)[0]
+txt2 = str(num)[1]
+txt3 = str(num)[2]
+
+res = int(txt1) + int(txt2)
+res2 = int(txt1) + int(txt2) + int(txt3)
+print(res) # выведет 3
+print(res2) # выведет 6
+
+txt = '12.1'
+print(float(txt)) # выведет 12.1
+
+tst1 = 2
+tst2 = 4
+res1 = float(tst1)
+res2 = float(tst2)
+print(res2 - res1)
+
+tst = input('Введите день недели: ')
+print(tst)
+
+print(int(tst) + tst1)
+
+
